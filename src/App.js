f98b0322576fe36492ee314585226b660f018f80
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-03 12:48:16 -0400

    Change app logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import mathLogo from './mathLogo.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={mathLogo} className="App-logo" alt="logo" />
         <h1>
           Math Magicians!
         </h1>
