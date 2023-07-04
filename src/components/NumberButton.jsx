285a09595239a174fc0ade9101c7bb1d8e9327d0
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-04 16:47:19 -0400

    Add PropTypes rules for the props

--- src/components/NumberButton.jsx
+++ src/components/NumberButton.jsx
@@ -1,1 +1,10 @@
-export const NumberButton = ({ name, classes }) => (<button className={`btn ${classes.join(' ')}`}>{ name }</button>);
+import PropTypes from 'prop-types';
+
+const NumberButton = ({ name, classes }) => (<button type="button" className={`btn ${classes.join(' ')}`}>{ name }</button>);
+
+NumberButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  classes: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default NumberButton;
--- src/components/OperatorButton.jsx
+++ src/components/OperatorButton.jsx
@@ -1,1 +1,10 @@
-export const OperatorButton = ({ name, classes }) => (<button className={`btn ${classes.join(' ')}`}>{ name }</button>);
+import PropTypes from 'prop-types';
+
+const OperatorButton = ({ name, classes }) => (<button type="button" className={`btn ${classes.join(' ')}`}>{ name }</button>);
+
+OperatorButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  classes: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default OperatorButton;
