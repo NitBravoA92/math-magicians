6319cdc7e429653351b7e2d27e3c81ea12fa7589
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-04 11:05:16 -0400

    Remove the header section and all unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-import mathLogo from './mathLogo.gif';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={mathLogo} className="App-logo" alt="logo" />
-        <h1>
-          Math Magicians!
-        </h1>
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
 
