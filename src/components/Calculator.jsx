bfdaa355681906bf097ab776311d5e9c73f8a574
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-05 14:47:28 -0400

    Remove the OperatorButton component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import calculate from '../logic/calculate';
 import './Calculator.css';
 import NumberButton from './NumberButton';
-import OperatorButton from './OperatorButton';
 
 const Calculator = () => {
 
@@ -26,25 +25,25 @@ const Calculator = () => {
       <p className="final-result"></p>
     </div>
     <div className="buttons-container">
-      <OperatorButton name="AC" classes={['btn-gray']} />
-      <OperatorButton name="+/-" classes={['btn-gray']} />
-      <OperatorButton name="%" classes={['btn-gray']} />
-      <OperatorButton name="÷" classes={['btn-orange']} />
+      <NumberButton name="AC" classes={['btn-gray']} />
+      <NumberButton name="+/-" classes={['btn-gray']} />
+      <NumberButton name="%" classes={['btn-gray']} />
+      <NumberButton name="÷" classes={['btn-orange']} />
       <NumberButton name="7" classes={['btn-gray']} />
       <NumberButton name="8" classes={['btn-gray']} />
       <NumberButton name="9" classes={['btn-gray']} />
-      <OperatorButton name="x" classes={['btn-orange']} />
+      <NumberButton name="x" classes={['btn-orange']} />
       <NumberButton name="4" classes={['btn-gray']} />
       <NumberButton name="5" classes={['btn-gray']} />
       <NumberButton name="6" classes={['btn-gray']} />
-      <OperatorButton name="-" classes={['btn-orange']} />
+      <NumberButton name="-" classes={['btn-orange']} />
       <NumberButton name="1" classes={['btn-gray']} />
       <NumberButton name="2" classes={['btn-gray']} />
       <NumberButton name="3" classes={['btn-gray']} />
-      <OperatorButton name="+" classes={['btn-orange']} />
+      <NumberButton name="+" classes={['btn-orange']} />
       <NumberButton name="0" classes={['btn-gray', 'btn-zero']} />
       <NumberButton name="." classes={['btn-gray']} />
-      <OperatorButton name="=" classes={['btn-orange']} />
+      <NumberButton name="=" classes={['btn-orange']} />
     </div>
   </section>
 )};
