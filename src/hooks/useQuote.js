700eb457f0b4b9fdbb19645fc0cd6603576373af
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-06 15:21:46 -0400

    Add a new a new state: renderState

--- src/hooks/useQuote.js
+++ src/hooks/useQuote.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const useQuote = (initialState) => {
   const [dataQuote, setDataQuote] = useState({
@@ -7,47 +7,46 @@ const useQuote = (initialState) => {
     error: null,
   });
 
-  const abortController = new AbortController();
+  const [renderState, setRenderState] = useState(false);
 
   const getQuote = async () => {
     const options = {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "X-Api-Key": "suyXArl0QxwZhSpQnumKzQ==NWZw3xiMxfy6yrAb",
+        'X-Api-Key': 'suyXArl0QxwZhSpQnumKzQ==NWZw3xiMxfy6yrAb',
       },
     };
 
     const response = await fetch(
-      "https://api.api-ninjas.com/v1/quotes?category=computers&limit=1",
-      options
+      'https://api.api-ninjas.com/v1/quotes?category=computers&limit=1',
+      options,
     );
-    
-    if(!response.ok) {
-      setDataQuote({ 
+
+    if (!response.ok) {
+      setDataQuote({
         loading: false,
         data: null,
-        error: 'Error in the request for a Quote!', 
+        error: 'Error in the request for a Quote!',
       });
       return;
     }
 
-    const [ dataResp ] = await response.json();
+    const [dataResp] = await response.json();
     const { quote, author } = dataResp;
 
-    setDataQuote({ 
+    setDataQuote({
       loading: false,
       data: { quote, author },
-      error: null, 
+      error: null,
     });
   };
 
-  useEffect(() => { getQuote(); }, []);
-
   useEffect(() => {
-    return () => {
-      abortController.abort();
-    };
-  }, [dataQuote]);
+    if (!renderState) {
+      getQuote();
+      setRenderState(!renderState);
+    }
+  }, [renderState]);
 
   return dataQuote;
 };
