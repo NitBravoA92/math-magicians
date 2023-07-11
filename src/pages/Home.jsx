fadca32724e72e4a07e6193bb16867ac47b57d6a
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-11 13:11:00 -0400

    Create the Home component that shows the main information of the webApp

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Layout from './Layout';
+
+const Home = () => (
+  <Layout>
+    <section id='home'>
+      <h1 className='page-title'>Welcome to our page!</h1>
+      <div className='page-content'>
+        <p className='home-info'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In iure amet architecto hic vel quia repellat recusandae nulla, tempora quo ea velit pariatur autem necessitatibus eius consequuntur porro quae delectus. Lorem ipsum dolor sit amet consectetur adipisicing elit. In iure amet architecto hic vel quia repellat recusandae nulla, tempora quo ea velit pariatur autem necessitatibus eius consequuntur porro quae delectus.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In iure amet architecto hic vel quia repellat recusandae nulla, tempora quo ea velit pariatur autem necessitatibus eius consequuntur porro quae delectus. Lorem ipsum dolor sit amet consectetur adipisicing elit. In iure amet architecto hic vel quia repellat recusandae nulla, tempora quo ea velit pariatur autem necessitatibus eius consequuntur porro quae delectus.
+        </p>
+        <p className='home-info'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In iure amet architecto hic vel quia repellat recusandae nulla, tempora quo ea velit pariatur autem necessitatibus eius consequuntur porro quae delectus. Lorem ipsum dolor sit amet consectetur adipisicing elit. In iure amet architecto hic vel quia repellat recusandae nulla, tempora quo ea velit pariatur autem necessitatibus eius consequuntur porro quae delectus.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In iure amet architecto hic vel quia repellat recusandae nulla, tempora quo ea velit pariatur autem necessitatibus eius consequuntur porro quae delectus. Lorem ipsum dolor sit amet consectetur adipisicing elit. In iure amet architecto hic vel quia repellat recusandae nulla, tempora quo ea velit pariatur autem necessitatibus eius consequuntur porro quae delectus.
+        </p>
+      </div>
+    </section>
+  </Layout>
+);
+
+export default Home;
