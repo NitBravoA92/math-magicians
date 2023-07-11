f4b63a1cc9a31c7e4a50ee171ca6a5d239fc3c7b
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-11 12:46:01 -0400

    Create the Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Logo from './Logo';
+import Navigation from './Navigation';
+
+const Header = () => (
+  <header>
+    <Logo />
+    <Navigation />
+  </header>
+);
+
+export default Header;
