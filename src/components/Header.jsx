2f4c3865da69c130296a524f8507a10573480227
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-11 19:55:43 -0400

    Add custom css styles to the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import Logo from './Logo';
 import Navigation from './Navigation';
+import '../styles/Header.css';
 
 const Header = () => (
   <header>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
 import mathLogo from '../mathLogo.gif';
 
 const Logo = () => (
-  <a href="/" id="logo-container">
+  <a href="/" className="logo-container">
     <img src={mathLogo} className="App-logo" alt="logo" />
-    <span id="main-title">Math Magicians</span>
+    <span className="logo-title">Math Magicians</span>
   </a>
 );
 
