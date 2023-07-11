e8d3612133261a4bb7e4d7b41c1ed35ec1b420b2
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-11 13:14:25 -0400

    Render the Home component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 );
 
