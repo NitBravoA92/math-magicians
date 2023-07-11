d0f6c9c5a4dc13b5b8a3a2c8b11f92769626b330
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-11 11:53:55 -0400

    Create the Logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import mathLogo from '../mathLogo.gif';
+const Logo = () => (
+  <div id="logo-container">
+    <img src={mathLogo} className="App-logo" alt="logo" />
+    <h1 id="main-title">Math Magicians</h1>
+  </div>
+);
+
+export default Logo;
