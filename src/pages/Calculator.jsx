f2a14b16eab9830831cfc6e7af743c3f3f6114e3
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-11 13:44:16 -0400

    Create the Calculator component to display the  Calculator machine

---
+++ src/pages/Calculator.jsx
@@ -0,0 +1,15 @@
+import Layout from './Layout';
+import CalculatorApp from '../components/CalculatorApp';
+
+const Calculator = () => (
+  <Layout>
+    <section id='calculator' className='d-column'>
+      <h1 className='page-title'>Let's do some math!</h1>
+      <div className='page-content'>
+        <CalculatorApp />
+      </div>
+    </section>
+  </Layout>
+);
+
+export default Calculator;
