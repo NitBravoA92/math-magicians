1d1c618378c00281b444b6d3275b28bf45df1c2d
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-11 21:58:17 -0400

    Add css styles for the Quote component

--- src/components/QuoteItem.jsx
+++ src/components/QuoteItem.jsx
@@ -5,9 +5,12 @@ const QuoteItem = () => {
 
   return (
     <div id="display-quote">
-      <p id="quote-text" className={`${JSON.stringify(data) === '{}' ? 'd-none' : ''}`}>
-        <i>{data?.quote || ''}</i>
-        <b>{data?.author || ''}</b>
+      <p id="quote-text" className={`${(JSON.stringify(data) === '{}' || data === null) ? 'd-none' : ''}`}>
+        <span id="quote">{data?.quote || ''}</span>
+        <span id="author">
+          {' — '}
+          {data?.author || ''}
+        </span>
       </p>
       <p id="loading-message" className={`${!loading ? 'd-none' : ''}`}>{ loading ? 'Loading Quote...' : '' }</p>
       <p id="error-message" className={`${!error ? 'd-none' : ''}`}>{ error || '' }</p>
--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -1,5 +1,6 @@
 import Layout from './Layout';
 import QuoteItem from '../components/QuoteItem';
+import '../styles/Quote.css';
 
 const Quote = () => (
   <Layout>
