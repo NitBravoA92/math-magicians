2b8abc99d43329bf5d6593de1254643db5920116
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-11 21:17:30 -0400

    Update css styles of the Calculator component

--- src/components/CalculatorApp.jsx
+++ src/components/CalculatorApp.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import calculate from '../logic/calculate';
-import '../styles/Calculator.css';
 import CalculatorButton from './CalculatorButton';
 
 const CalculatorApp = () => {
--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,9 +1,10 @@
 import Layout from './Layout';
 import CalculatorApp from '../components/CalculatorApp';
+import '../styles/Calculator.css';
 
 const Calculator = () => (
   <Layout>
-    <section id="calculator" className="d-column">
+    <section id="calculator">
       <h1 className="page-title">Let&apos;s do some math!</h1>
       <div className="page-content">
         <CalculatorApp />
