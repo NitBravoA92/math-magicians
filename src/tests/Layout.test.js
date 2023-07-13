2bbe116c63c5c647ec6c5ba5b296630772ec42de
Reza Merzaie <ghrezam97@gmail.com>
2023-07-13 04:33:36 +0430

    Add test for Layout component

---
+++ src/tests/Layout.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import renderer from 'react-test-renderer';
+import Layout from "../pages/Layout";
+import Header from "../components/Header";
+
+describe("The Layout component", () => {
+
+  test("renders correctly into the DOM", () => {
+    const component = renderer
+    .create(
+      <Layout>
+        <Header />
+        <main>
+          <section id="home">
+            <h1 className="page-title">Welcome to our page!</h1>
+          </section>
+        </main>
+      </Layout>)
+    .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+});
