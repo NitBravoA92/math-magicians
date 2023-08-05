6023e0186e0f46d1706967f11daec310b7495e33
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-05 16:20:18 -0400

    Fix linters issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,18 @@ const Home = () => (
       <h1 className="page-title">Welcome to our page!</h1>
       <div className="page-content">
         <p className="home-info">
-          Math Magicians is a web application aimed at lovers of mathematics and calculators. 
-          We can say that calculations are a fundamental part of our life. 
-          We experience them in every aspect of our lives; children, adults, or in our work. 
-          However, some calculations are easy but there may be some that are somewhat more 
-          complicated for us and we need the help of a tool that quickly solves them for us. 
-          That's where the beloved calculators come in.
+          Math Magicians is a web application aimed at lovers of mathematics and calculators.
+          We can say that calculations are a fundamental part of our life.
+          We experience them in every aspect of our lives; children, adults, or in our work.
+          However, some calculations are easy but there may be some that are somewhat more
+          complicated for us and we need the help of a tool that quickly solves them for us.
+          That&apos;s where the beloved calculators come in.
         </p>
         <p className="home-info">
-          Calculators are a fundamental part of our lives and most of us have to use them at one point or another. 
-          Those people who are highly dependent on calculators can reduce their dependency by switching to using an online 
+          Calculators are a fundamental part of our lives and most of us have to
+          use them at one point or another.
+          Those people who are highly dependent on calculators can reduce
+          their dependency by switching to using an online
           calculator such as the one provided by this website.
         </p>
       </div>
